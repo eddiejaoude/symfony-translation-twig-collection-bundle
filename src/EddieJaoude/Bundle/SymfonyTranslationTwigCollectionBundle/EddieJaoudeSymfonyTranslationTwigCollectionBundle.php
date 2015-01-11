4cd4249fe6d487acdd974b4b2ee321f5863e1408
Eddie Jaoude <eddie@jaoudestudios.com>
2015-01-11 08:04:44 +0000

    Symfony bundle files

---
+++ src/EddieJaoude/Bundle/SymfonyTranslationTwigCollectionBundle/EddieJaoudeSymfonyTranslationTwigCollectionBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace EddieJaoude\Bundle\SymfonyTranslationTwigCollectionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class EddieJaoudeSymfonyTranslationTwigCollectionBundle
+ * @package EddieJaoude\Bundle\SymfonyTranslationTwigCollectionBundle
+ */
+class EddieJaoudeSymfonyTranslationTwigCollectionBundle extends Bundle
+{
+}
